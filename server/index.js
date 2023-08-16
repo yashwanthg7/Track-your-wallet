34b63c363ae2220e38b32c93de08adfce0a1e188
yashwanthg7 <yashwanth18183@gmail.com>
2023-08-16 22:55:29 +0530

    Deploy error resolved

--- server/index.js
+++ server/index.js
@@ -13,16 +13,16 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT
 app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://track-your-wallet.onrender.com/"
-    ); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader(
+//       "Access-Control-Allow-Origin",
+//       "https://track-your-wallet.onrender.com/"
+//     ); 
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     next();
+// });
 
 app.use(cookieParser());
 
